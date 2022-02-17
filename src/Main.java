30bd56b26da56f1db8ca9f36f5af98d23ee49b6a
MarinaFriauf <marina506506506@gmail.com>
2022-02-17 23:05:18 +0700

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int price = 10_000;
+        int x = 20;
+        int miles = price / x;
+        System.out.println("Начисленные мили:" + miles);
     }
 }
